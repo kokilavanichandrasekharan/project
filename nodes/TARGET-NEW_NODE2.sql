31fbe1b1eb78b3b2af2352fd813f0ac44eaec377
project <kokilavani.chandrasekharan@dataentrega.com>
2026-07-15 04:35:19 +0530

    test000

--- nodes/TARGET-NEW_NODE2.sql
+++
@@ -1,7 +0,0 @@
-@id("1dc3e031-bcb8-4acb-af1c-2e64c71fa90e")
-@nodeType("24")
-@materializationType("table")
-
-SELECT
-    N_NAME::VARCHAR(20) AS NAME @collate('en-cs')
-FROM {{ ref('SOURCE','NATION_TEST') }}
